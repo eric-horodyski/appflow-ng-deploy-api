abdde2b72509e7779388a4bb749c6d364d7c1706
Eric Horodyski <horodyski@ionic.io>
2021-11-09 11:29:21 -0500

    Add some logging.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Deploy } from 'cordova-plugin-ionic';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  async ngOnInit() {
+    const info = await Deploy.getCurrentVersion();
+    console.log('Current Version', info);
+    const update = await Deploy.checkForUpdate();
+    console.log('Is an update available?', update);
+  }
 }
